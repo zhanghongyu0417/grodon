0e44178871db270632d6c5b6695adc3ebf9c447b
zhanghongyu <1522720639@qq.com>
2018-09-08 21:43:00 +0800

    配置首页

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+// node.js的模块化遵守CommonJS规范
+// var Vue = require('vue')
+
+// ES6模块化的语法
+import Vue from 'vue';
+import App from './App.vue';
+
+// 导入全局的样式
+// 导入bootstrap的样式
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.css';
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    render: h => h(App)
+});
